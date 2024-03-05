7b028cbb12812a2d0606c51e3942047132db3219
Michele <g.gambaro@outlook.com>
2024-03-05 23:48:35 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+#define SCR_WIDTH 1280
+#define SCR_HEIGHT 720
+
+GLFWwindow* init() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Asteroids2", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(1);
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        exit(1);
+    }
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    return window;
+}
+
+int main() {
+    GLFWwindow* window = init();
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(1, 1, 1, 1);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
